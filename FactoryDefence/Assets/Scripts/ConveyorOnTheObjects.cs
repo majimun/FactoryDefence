4f72ee906eb3ae5a0ec3a62cc3e6a2d17eaad7ab
majimun <yanbarumajimun@gmail.com>
2015-12-17 18:46:38 +0900

    Boxクリック途中経過

---
+++ FactoryDefence/Assets/Scripts/ConveyorOnTheObjects.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyorOnTheObjects : MonoBehaviour {
+
+    // 接触しているキャラクタを左から右へ流す
+    public void OnCollisionStay(Collision other) {
+        if(other.gameObject.tag.Equals("Box")) {
+            Vector3 pos = other.transform.position;
+            pos += transform.right * Time.deltaTime * 1.0f;
+            other.transform.position = pos;
+        }
+    }
+}
---
+++ FactoryDefence/Assets/Scripts/GenerateBoxInObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateBoxInObject : MonoBehaviour {
+
+    public GameObject InObject;     // 出現させるオブジェクト
+
+    // 出現時初期化メソッド
+    void Start () {
+	    
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetMouseButtonDown(0)) {
+            Debug.Log("generate box");
+        }
+	}
+}
